5c72d486a1528710bff7111dd9625309fa676866
Vovchic170 <95353550+Vovchic170@users.noreply.github.com>
2025-05-22 00:48:42 +0000

    some changes

--- src/router/index.js
+++ src/router/index.js
@@ -18,3 +18,23 @@ export default router
 
 
 
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import LandingPage from '../views/LandingPage.vue';
+import BooksPage from '../views/BooksPage.vue';
+import BasketPage from '../views/BasketPage.vue';
+
+const routes = [
+  { path: '/', component: LandingPage },
+  { path: '/books', component: BooksPage },
+  { path: '/basket', component: BasketPage }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
+
+
