104aeba1719be2f3bc8fbd8fe2ab1cb7cb1aeeca
Vovchic170 <95353550+Vovchic170@users.noreply.github.com>
2025-06-01 20:42:47 +0000

    restart

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  
   server: {
-    host: '0.0.0.0',
-    port: 5173
+    allowedHosts: [
+      '1b91eacd-f513-46f0-a936-8555a2dd14a5-00-18tvnpwtg30ga.pike.replit.dev'
+    ]
   }
 })
